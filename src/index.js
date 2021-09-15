e8da171e53e93b40466c769ecb9debc7dca5dc50
Roberto Oliveros <robertooliveros@mac.com>
2021-09-15 12:38:25 -0600

    Render hello world

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import path from 'path';
 
 const server = express();
 const port   = 3040;
 
-server.get('/', (req, res) => res.json({ value: 'Hello world!' }));
+server.set('view engine', 'ejs');
+server.set('views', path.join(__dirname, '/views'));
+
+server.get('/', (req, res) => res.render('index'));
 
 server.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);

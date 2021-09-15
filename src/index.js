2be5f5897c89ca6ab9a22bf811b7e0c5f8309b21
Roberto Oliveros <robertooliveros@mac.com>
2021-09-15 12:58:56 -0600

    Render images

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import express from 'express';
 import path from 'path';
+import fs from 'fs';
 
 const server = express();
 const port   = 3040;
+const directoryPath = path.join(process.env.PICTURES_DIR);
 
 server.set('view engine', 'ejs');
 server.set('views', path.join(__dirname, '/views'));
+server.use(express.static(directoryPath));
 
-server.get('/', (req, res) => res.render('index'));
+server.get('/', (req, res) => {
+  fs.readdir(directoryPath, (err, files) => {
+    //handling error
+    if (err) {
+      console.log('Unable to scan directory: ' + err);
+      return res.sendStatus(500);
+    }
+
+    let pictures = [];
+
+    files.forEach((file) => {
+      if(path.extname(file) === '.jpg' || path.extname(file) === '.jpeg' || path.extname(file) === '.png') {
+        pictures.push(file);
+      }
+    });
+
+    res.render('index', { pictures });
+  });
+});
 
 server.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
